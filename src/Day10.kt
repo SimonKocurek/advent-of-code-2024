87bf7d823af17fb20226cfa6f0ceac42f380b8e3
Šimon Kocúrek <15381356+SimonKocurek@users.noreply.github.com>
2024-12-10 20:17:46 +0100

    Day 10

---
+++ src/Day10.kt
@@ -0,0 +1,113 @@
+import java.util.Stack
+
+fun main() {
+
+    val directions = listOf(
+        0 to 1,
+        0 to -1,
+        1 to 0,
+        -1 to 0,
+    )
+
+    fun getZeros(map: List<List<Int>>): MutableList<Pair<Int, Int>> {
+        val zeros = mutableListOf<Pair<Int, Int>>()
+        for (y in map.indices) {
+            for (x in map[y].indices) {
+                if (map[y][x] == 0) {
+                    zeros.add(y to x)
+                }
+            }
+        }
+        return zeros
+    }
+
+    fun part1(input: List<String>): Int {
+        val map = input.map { line -> line.map { it.digitToInt() } }
+        val zeros = getZeros(map)
+
+        var result = 0
+
+        for ((startY, startX) in zeros) {
+            val seen = map.map { line -> line.map { false }.toMutableList() }
+
+            val following = Stack<Pair<Int, Int>>().apply {
+                add(startY to startX)
+                seen[startY][startX] = true
+            }
+
+            while (following.isNotEmpty()) {
+                val (y, x) = following.pop()
+                for ((diffY, diffX) in directions) {
+                    val newY = y + diffY
+                    val newX = x + diffX
+
+                    if (newY < 0 || newY >= map.size || newX < 0 || newX >= map[newY].size) {
+                        continue
+                    }
+
+                    if (seen[newY][newX]) {
+                        continue
+                    }
+
+                    if (map[newY][newX] - 1 != map[y][x]) {
+                        continue
+                    }
+
+                    seen[newY][newX] = true
+                    if (map[newY][newX] == 9) {
+                        result++
+                    } else {
+                        following.add(newY to newX)
+                    }
+                }
+            }
+        }
+
+        return result
+    }
+
+    fun part2(input: List<String>): Long {
+        val map = input.map { line -> line.map { it.digitToInt() } }
+
+        val waysToReach = map.map { line -> line.map { 0 }.toMutableList() }
+        getZeros(map).forEach { (y, x) -> waysToReach[y][x] = 1 }
+
+        var result = 0L
+        for (i in 1 .. 9) {
+
+            for (y in map.indices) {
+                for (x in map[y].indices) {
+
+                    if (map[y][x] != i) {
+                        continue
+                    }
+
+                    for ((diffY, diffX) in directions) {
+                        val fromY = y + diffY
+                        val fromX = x + diffX
+
+                        if (fromY < 0 || fromY >= map.size || fromX < 0 || fromX >= map[fromY].size) {
+                            continue
+                        }
+
+                        if (map[fromY][fromX] + 1 != map[y][x]) {
+                            continue
+                        }
+
+                        waysToReach[y][x] += waysToReach[fromY][fromX]
+                        if (i == 9) {
+                            result += waysToReach[fromY][fromX]
+                        }
+                    }
+
+                }
+            }
+        }
+
+        return result
+    }
+
+    val input = readInput("Day10")
+    part1(input).println()
+    part2(input).println()
+}
