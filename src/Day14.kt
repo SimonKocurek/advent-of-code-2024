6be8e6717105686c22f8ca2ac1ee8c8e82f13bbc
Šimon Kocúrek <15381356+SimonKocurek@users.noreply.github.com>
2024-12-14 12:18:33 +0100

    Day 14 - Improve detection

--- src/Day14.kt
+++ src/Day14.kt
@@ -1,7 +1,9 @@
 import java.awt.image.BufferedImage
 import java.awt.image.BufferedImage.TYPE_INT_RGB
 import java.io.File
+import java.util.*
 import javax.imageio.ImageIO
+import kotlin.math.sqrt
 
 fun main() {
 
@@ -67,22 +69,39 @@ fun main() {
     fun part2(input: List<String>) {
         val robots = parseRobots(input)
 
-        for (i in 1..10_000) {
-            val image = BufferedImage(maxX, maxY, TYPE_INT_RGB)
+        fun Robot.nearestRobots(robots: List<Robot>) =
+            PriorityQueue(
+                robots.map { other ->
+                    sqrt((x - other.x) * (x - other.x) + (y - other.y) * (y - other.y).toDouble()).toLong()
+                }
 
-            robots
-                .onEach { it.move() }
-                // Drawing a single point didn't work, so we do with a circle of size 1
-                .forEach { image.setRGB(it.x, it.y, (255 * 65536) + (255 * 256) + 255) }
+                // When robots are clumped up, the closest ones will be very close
+                // If they are scattered, the closest ones will be fairly far away
+            ).take(10).sum()
 
-            File("day14/$i.jpg").apply {
-                createNewFile()
-                ImageIO.write(image, "jpg", this)
-            }
+        fun List<Robot>.sparsity() = PriorityQueue(
+            map { it.nearestRobots(this) }
+        )
+            // To eliminate outliers and detect clumping, we only consider the closest 20%
+            .take(size / 5).sum()
+
+        var i = 0
+        while (robots.sparsity() > 1300) {
+            i++
+            robots.forEach { it.move() }
         }
+
+        val image = BufferedImage(maxX, maxY, TYPE_INT_RGB).apply {
+            robots.forEach { setRGB(it.x, it.y, (255 * 65536) + (255 * 256) + 255) }
+        }
+
+        val file = File("day14/$i.jpg").apply {
+            createNewFile()
+        }
+        ImageIO.write(image, "jpg", file)
     }
 
     val input = readInput("Day14")
     part1(input).println()
-    part2(input).println()
+    part2(input)
 }
