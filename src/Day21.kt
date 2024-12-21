1e0189e311b9dd2452afe502db0ff1bcfecdaece
Šimon Kocúrek <15381356+SimonKocurek@users.noreply.github.com>
2024-12-21 15:20:57 +0100

    Day 21 - Naive speedup?

--- src/Day21.kt
+++ src/Day21.kt
@@ -14,8 +14,8 @@ fun main() {
         return result
     }
 
-    fun Map<Char, Pair<Int, Int>>.toMoveOptions(): Map<Pair<Char, Char>, List<String>> {
-        val result = mutableMapOf<Pair<Char, Char>, MutableSet<String>>()
+    fun Map<Char, Pair<Int, Int>>.toMoves(): Map<Pair<Char, Char>, String> {
+        val result = mutableMapOf<Pair<Char, Char>, String>()
 
         val (invalidY, invalidX) = this[' ']!!
 
@@ -34,10 +34,23 @@ fun main() {
                 val dX = fromX - toX
                 val dY = fromY - toY
 
-                val paths = result.getOrPut(fromChar to toChar) { mutableSetOf() }
-
                 val stepsX = (if (dX < 0) ">" else "<").repeat(abs(dX))
                 val stepsY = (if (dY < 0) "v" else "^").repeat(abs(dY))
+
+                // In the robot keypad, we want to end up the closest
+                // to 'A' at the end of the move:
+                // - < is the furthest from 'A'
+                // - v is the second furthest
+                // - ^ and > are equally distant... but `>` has better movement options
+                val ordering = mapOf(
+                    null to -1,
+                    '<' to 0,
+                    'v' to 1,
+                    '^' to 2,
+                    '>' to 3,
+                )
+
+                val paths = mutableListOf<String>()
 
                 // Moving in X first is only possible, when we wouldn't move through invalid point
                 if (!(fromY == invalidY && toX == invalidX)) {
@@ -46,10 +59,17 @@ fun main() {
                 if (!(fromX == invalidX && toY == invalidY)) {
                     paths.add(stepsY + stepsX)
                 }
+
+                result[fromChar to toChar] = paths.sortedWith(
+                    compareBy(
+                        { it.length },
+                        { ordering[it.firstOrNull()] },
+                    )
+                ).first()
             }
         }
 
-        return result.mapValues { it.value.toList() }
+        return result
     }
 
     val keypad = listOf(
@@ -57,47 +77,38 @@ fun main() {
         "456",
         "123",
         " 0A",
-    ).toPointMap().toMoveOptions()
+    ).toPointMap().toMoves()
 
     val robotKeypad = listOf(
         " ^A",
         "<v>",
-    ).toPointMap().toMoveOptions()
+    ).toPointMap().toMoves()
 
-    fun shortestOptions(moveOptions: Map<Pair<Char, Char>, List<String>>, sequence: String): List<String> {
-        var options = mutableSetOf("")
-
+    fun shortestOptions(moveOptions: Map<Pair<Char, Char>, String>, sequence: String) = buildString {
         // We start at "A", so the first transition should be from 'A' to the first character
         ("A" + sequence).zipWithNext { fromChar, toChar ->
-            val newOptions = mutableSetOf<String>()
-
-            moveOptions[fromChar to toChar]?.forEach { added ->
-                options.forEach { previous ->
-                    newOptions.add(previous + added + "A")
-                }
-            }
-
-            options = newOptions
+            append(moveOptions[fromChar to toChar])
+            append("A")
         }
-
-        val shortestOptionLength = options.minOf { it.length }
-        return options.filter { it.length == shortestOptionLength }
     }
 
-    fun part1() = input.sumOf { line ->
-        val length = shortestOptions(keypad, line).minOf { generatedLine ->
-            shortestOptions(robotKeypad, generatedLine).minOf { secondGenerated ->
-                shortestOptions(robotKeypad, secondGenerated).minOf {
-                    it.length
-                }
-            }
+    fun combinationValue(combination: String, robots: Int): Long {
+        val shortestKeypadClicks = shortestOptions(keypad, combination)
+
+        var shortestOption = shortestKeypadClicks
+        for (robot in 1..robots) {
+            shortestOption = shortestOptions(robotKeypad, shortestOption)
         }
 
-        length * line.takeWhile { it.isDigit() }.toInt()
+        val digitValue = combination.takeWhile { it.isDigit() }.toLong()
+
+        return shortestOption.length * digitValue
     }
 
-    fun part2() = 0
+    fun part1() = input.sumOf { combinationValue(combination = it, robots = 2) }
+
+    fun part2() = input.sumOf { combinationValue(combination = it, robots = 25) }
 
     part1().println()
-    part2().println()
+//    part2().println()
 }
