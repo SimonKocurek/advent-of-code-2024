7a53dc2f1223ffb21ac61e6e6e3913f461e861ba
Šimon Kocúrek <15381356+SimonKocurek@users.noreply.github.com>
2024-12-23 10:19:57 +0100

    Day 23 - pt2

--- src/Day23.kt
+++ src/Day23.kt
@@ -3,7 +3,7 @@ fun main() {
     val input = readInput("Day23")
         .map { it.split("-") }
 
-    fun buildGraph() = buildMap<String, MutableSet<String>> {
+    fun buildGraph(): Map<String, Set<String>> = buildMap<String, MutableSet<String>> {
         input.forEach { (from, to) ->
             getOrPut(from) { mutableSetOf() }.add(to)
             getOrPut(to) { mutableSetOf() }.add(from)
@@ -12,22 +12,48 @@ fun main() {
 
     val graph = buildGraph()
 
-    fun part1() = buildSet {
-        graph.forEach { (a, aNeighbours) ->
+    fun joinOneToLanParty(groups: Map<Set<String>, Set<String>>) = buildMap<Set<String>, Set<String>> {
+        groups.forEach { (aGroup, aNeighbours) ->
             aNeighbours.forEach { b ->
+                val entry = aGroup + b
+                if (entry in this) return@forEach
+
                 val bNeighbours = graph[b]!!
-                bNeighbours.forEach { c ->
-                    if (c in aNeighbours) {
-                        if (a.startsWith("t") || b.startsWith("t") || c.startsWith("t")) {
-                            add(listOf(a, b, c).sorted().joinToString(","))
-                        }
-                    }
-                }
+                val commonNeighbours = aNeighbours.intersect(bNeighbours)
+                this[entry] = commonNeighbours
             }
         }
-    }.count()
+    }
 
-    fun part2() = 0
+    fun part1(): Int {
+        val groupsOf2 = joinOneToLanParty(groups = graph.mapKeys { setOf(it.key) })
+        val groupsOf3 = joinOneToLanParty(groups = groupsOf2)
+
+        return groupsOf3
+            .keys
+            .filter { parts -> parts.any { it.startsWith("t") } }
+            .count()
+    }
+
+    fun part2(): String {
+        var lanParty = graph.mapKeys { setOf(it.key) }
+
+        while (true) {
+            val newLanParty = joinOneToLanParty(groups = lanParty)
+
+            if (newLanParty.isEmpty()) {
+                break
+            }
+
+            lanParty = newLanParty
+        }
+
+        return lanParty
+            .keys
+            .first()
+            .sorted()
+            .joinToString(",")
+    }
 
     part1().println()
     part2().println()
