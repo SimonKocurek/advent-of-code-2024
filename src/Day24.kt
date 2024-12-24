df4fa6465041045c4dbd2e70f439233ef7d0ae84
Šimon Kocúrek <15381356+SimonKocurek@users.noreply.github.com>
2024-12-24 14:45:12 +0100

    Day 24 - pt2 - manual solution

--- src/Day24.kt
+++ src/Day24.kt
@@ -1,22 +1,26 @@
+import kotlin.experimental.and
+import kotlin.experimental.or
+import kotlin.experimental.xor
+
 fun main() {
 
     data class Gate(
         val a: String,
         val b: String,
-        val op: (a: Long, b: Long) -> Long,
+        val op: (a: Byte, b: Byte) -> Byte,
         val out: String
     )
 
-    fun and(a: Long, b: Long) = a and b
-    fun or(a: Long, b: Long) = a or b
-    fun xor(a: Long, b: Long) = a xor b
+    fun and(a: Byte, b: Byte) = a and b
+    fun or(a: Byte, b: Byte) = a or b
+    fun xor(a: Byte, b: Byte) = a xor b
 
     val input = readInput("Day24")
 
-    val values = input
+    val inputValues = input
         .takeWhile { it.isNotEmpty() }
         .map { it.split(": ") }
-        .associateBy({ it[0] }, { it[1].toLong() })
+        .associateBy({ it[0] }, { it[1].toByte() })
 
     val gates = input
         .takeLastWhile { it.isNotEmpty() }
@@ -36,31 +40,125 @@ fun main() {
         }
         .associateBy { it.out }
 
-    fun part1(): Long {
-        val cache = values.toMutableMap()
+    fun MutableMap<String, Byte>.getMemoized(output: String): Byte {
+        if (output !in this) {
+            val gate = gates[output]!!
 
-        fun getValue(output: String): Long {
-            if (output !in cache) {
-                val gate = gates[output]!!
-
-                val a = getValue(gate.a)
-                val b = getValue(gate.b)
-                cache[output] = gate.op(a, b)
-            }
-
-            return cache[output]!!
+            val a = getMemoized(gate.a)
+            val b = getMemoized(gate.b)
+            this[output] = gate.op(a, b)
         }
 
-        return gates
-            .keys
-            .filter { it.startsWith("z") }
-            .map { it.substring(1).toInt() to getValue(it) }
-            .sortedByDescending { it.first }
-            .joinToString("") { (_, value) -> value.toString() }
-            .toLong(radix = 2)
+        return this[output]!!
     }
 
-    fun part2() = 0
+    fun getDigits(name: String, cache: MutableMap<String, Byte>) = gates
+        .keys
+        .filter { it.startsWith(name) }
+        .map { it.substring(1).toInt() to cache.getMemoized(it) }
+        .sortedByDescending { it.first }
+        .map { it.second }
+
+    fun List<Byte>.digitsToLong() = joinToString("") { it.toString() }.toLong(radix = 2)
+
+    fun getCircuitResult(values: Map<String, Byte>): Long {
+        val cache = values.toMutableMap()
+
+        return getDigits(name = "z", cache).digitsToLong()
+    }
+
+    fun validate() {
+        // Helps find errors in the manually fixed circuit
+        for (i in 0..1000) {
+            val randomInputs = inputValues.toMutableMap().apply {
+                for (key in keys) {
+                    this[key] = (0..1).random().toByte()
+                }
+            }
+
+            val result = getCircuitResult(values = randomInputs)
+
+            val x = randomInputs
+                .filterKeys { it.startsWith("x") }
+                .mapKeys { it.key.substring(1).toInt() }
+                .toList()
+                .sortedByDescending { it.first }
+                .map { it.second }
+                .digitsToLong()
+
+            val y = randomInputs
+                .filterKeys { it.startsWith("y") }
+                .mapKeys { it.key.substring(1).toInt() }
+                .toList()
+                .sortedByDescending { it.first }
+                .map { it.second }
+                .digitsToLong()
+
+            if (result != x + y) {
+                val got = result.toString(radix = 2)
+                val expected = (x + y).toString(radix = 2)
+
+                error("Validation failed - got: $got != $expected :expected")
+            }
+        }
+    }
+
+    fun part1() = getCircuitResult(values = inputValues)
+
+    fun part2(): String {
+        val cache = inputValues.toMutableMap()
+        getDigits(name = "z", cache) // Populate the cache
+
+        fun Gate.opName() = when (op) {
+            ::and -> "AND"
+            ::or -> "OR"
+            ::xor -> "XOR"
+            else -> error("Unknown operator")
+        }
+
+        fun Gate.opClass() = when (op) {
+            ::and -> "add"
+            ::or -> "or"
+            ::xor -> "xor"
+            else -> error("Unknown operator")
+        }
+
+        fun Gate.name() = "${a}_${opName()}_${b}:::${opClass()}"
+
+        // It's easier to correct the errors by hand than to write a program to do it
+        return buildString {
+            appendLine("stateDiagram-v2")
+
+            appendLine("\tclassDef output fill:#f00,color:white,font-weight:bold,stroke-width:2px,stroke:yellow")
+
+            appendLine("\tclassDef add stroke-width:2px,stroke:red")
+            appendLine("\tclassDef or stroke-width:2px,stroke:green")
+            appendLine("\tclassDef xor stroke-width:2px,stroke:blue")
+
+            fun String.keyClass() = if (first() == 'z') ":::output" else ""
+
+            gates.forEach { (_, gate) ->
+                // Some gates may not be used
+                if (gate.a !in cache) {
+                    return@forEach
+                }
+                if (gate.b !in cache) {
+                    return@forEach
+                }
+
+                val operation = gate.name()
+
+                if (!gate.a.startsWith("x") && !gate.a.startsWith("y")) {
+                    appendLine("\t${gate.a} --> $operation")
+                }
+                if (!gate.b.startsWith("x") && !gate.b.startsWith("y")) {
+                    appendLine("\t${gate.b} --> $operation")
+                }
+
+                appendLine("\t${operation} --> ${gate.out}${gate.out.keyClass()}")
+            }
+        }
+    }
 
     part1().println()
     part2().println()
